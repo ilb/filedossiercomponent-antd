ed7f0e95c3fc018de0316faf2ce7d59972f06afb
MaximumQuiet <contact@maximumquiet.cyou>
2021-09-15 16:42:12 +0400

    Refactor: rename old dossier component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import FileDossier from './classes/FileDossier';
-import DossierComponent from './components/Dossier';
+import DossierComponent from './components/DossierComponent';
 import ExternalFilesList from './components/ExternalDossier/FilesList';
 
 export { FileDossier, DossierComponent, ExternalFilesList };
