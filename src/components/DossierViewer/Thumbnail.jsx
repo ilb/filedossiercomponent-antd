92b2bc5681f9da83422604c1de4a1cbfd67feb1a
Bonamente <56506035@protonmail.com>
2023-12-26 10:56:17 +0500

    feat(Thumbnail): move component to Antd

--- src/components/DossierViewer/Thumbnail.jsx
+++ src/components/DossierViewer/Thumbnail.jsx
@@ -5,10 +5,17 @@ export default function Thumbnail({ file, images, sizes: { width, height } = {} 
   return (
     <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
       <img
-        src={images[0].src} // key={images[0].src}
-        className={`ui fluid image file-dossier-img-rotate${file.rotate || 0}`}
-        style={{ maxWidth: `${width}px`, maxHeight: `${height}px` }}
+        src={images[0].src}
+        className={`file-dossier-img-rotate${file.rotate || 0}`}
+        style={{
+          display: 'block',
+          width: '100%',
+          height: 'auto',
+          maxWidth: `${width}px`,
+          maxHeight: `${height}px`
+        }}
         // alt="Не удалось отобразить preview файла"
+        alt=""
       />
     </div>
   );
