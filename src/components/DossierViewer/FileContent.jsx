38b11a3038850b676c77c40807859da52ffc81af
Bonamente <56506035@protonmail.com>
2023-12-26 10:58:59 +0500

    feat(FileContent): move component to Antd

--- src/components/DossierViewer/FileContent.jsx
+++ src/components/DossierViewer/FileContent.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Segment, Message, Menu } from 'semantic-ui-react';
+
 import Thumbnail from './Thumbnail';
 import ImagesViewer from './ImagesViewer';
 import DossierInfo from './ImagesViewer/DossierInfo';
 import FileDossier from '../../classes/FileDossier';
+
+import { Alert, Card } from 'antd';
 
 export default function FileContent({ basePath, file, mode, sizes }) {
   const dossierInst = new FileDossier({ basePath });
@@ -14,6 +16,7 @@ export default function FileContent({ basePath, file, mode, sizes }) {
     error: null,
     value: null
   });
+
   const getContainer = dossierInst.createAction({
     description: 'Загрузка контейнера файла',
     state: containerState,
@@ -21,18 +24,19 @@ export default function FileContent({ basePath, file, mode, sizes }) {
     action: dossierInst.getContainer
   });
 
-  const createErrorMessage = ({ header, content, warning, attached }) => {
+  const createErrorMessage = ({ header, content, warning }) => {
     // eslint-disable-line react/prop-types
     const thumbnailErrorStyle = { margin: 0, position: 'absolute', top: 0, left: 0, right: 0 };
+
+    const alertType = warning ? 'warning' : 'error';
+
     return (
-      <Message
-        error={!warning}
-        warning={warning}
-        header={mode === 'thumbnail' ? 'Ошибка' : header}
-        content={mode === 'thumbnail' ? undefined : content}
+      <Alert
         style={mode === 'thumbnail' ? thumbnailErrorStyle : undefined}
-        attached={attached}
-        title={`${header}. ${content || ''}`}
+        message={mode === 'thumbnail' ? <b>Ошибка</b> : <b>{header}</b>}
+        description={mode === 'thumbnail' ? undefined : content}
+        type={alertType}
+        showIcon
       />
     );
   };
@@ -85,6 +89,7 @@ export default function FileContent({ basePath, file, mode, sizes }) {
   }
 
   let ImagesComponent;
+
   switch (mode) {
     case 'thumbnail':
       ImagesComponent = Thumbnail;
@@ -92,18 +97,17 @@ export default function FileContent({ basePath, file, mode, sizes }) {
     default:
       ImagesComponent = ImagesViewer;
   }
+
   const contentRef = useRef(null);
 
   return (
-    <Segment
-      basic
+    <Card
       loading={containerState.loading}
       className="file-dossier-file-content"
       style={{
         position: 'relative',
         width: '100%',
         height: '100%',
-        padding: 0,
         margin: 0,
         minHeight: '46px'
       }}>
@@ -121,27 +125,21 @@ export default function FileContent({ basePath, file, mode, sizes }) {
         images.length === 0 &&
         createErrorMessage({
           warning: true,
-          header: `Не найдены изображения в контейнере`,
-          attached: mode !== 'thumbnail' ? 'top' : undefined
+          header: `Не найдены изображения в контейнере`
         })}
       {!canDisplayFile &&
         createErrorMessage({
-          header: `Невозможно отобразить файл: ${file.name}`,
-          attached: mode !== 'thumbnail' ? 'top' : undefined
+          header: `Невозможно отобразить файл: ${file.name}`
         })}
       {!!containerState.error &&
         createErrorMessage({
           header: 'Ошибка при загрузке контейнера',
-          content: containerState.error,
-          attached: mode !== 'thumbnail' ? 'top' : undefined
+          content: containerState.error
         })}
+
       {(!canDisplayFile || !!containerState.error || (images && images.length === 0)) &&
-        mode !== 'thumbnail' && (
-          <Menu vertical attached="bottom" style={{ width: 'auto' }}>
-            <DossierInfo file={file} />
-          </Menu>
-        )}
-    </Segment>
+        mode !== 'thumbnail' && <DossierInfo file={file} />}
+    </Card>
   );
 }
 
